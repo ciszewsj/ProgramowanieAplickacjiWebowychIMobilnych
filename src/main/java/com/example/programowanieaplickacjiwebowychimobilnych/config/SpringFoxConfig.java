7ee5433eb21484525416db8157a04c026172eedc
jciszewski <jciszewski@amusys.pl>
2022-11-19 18:58:08 +0100

    add frontend

--- src/main/java/com/example/programowanieaplickacjiwebowychimobilnych/config/SpringFoxConfig.java
+++
@@ -1,8 +0,0 @@
-package com.example.programowanieaplickacjiwebowychimobilnych.config;
-
-import org.springframework.context.annotation.Configuration;
-
-
-@Configuration
-public class SpringFoxConfig {
-}
